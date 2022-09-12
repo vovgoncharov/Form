4554d6c50bca694349df0d10ad594daa4337e647
Vova <deykdog@gmail.com>
2022-09-13 00:38:49 +0300

    done

--- script.js
+++ script.js
@@ -5,40 +5,47 @@ const fName = document.querySelector(".first-name");
 const lName = document.querySelector(".last-name");
 const mob = document.querySelector(".mobile");
 const pass = document.querySelector(".pass");
+const calendar = document.querySelector(".date");
+
+const getDate = () => {
+  let date = new Date().toLocaleDateString();
+  let dateToday = (calendar.innerHTML = `${date}`);
+  return dateToday;
+};
+console.log(getDate());
 const inputs = [fName, lName, mob, pass];
 const getForm = () => {
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        text.innerHTML = "";
-        text.innerHTML = `${fName.value} ${lName.value}, ${mob.value},${pass.value}`;
-    });
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    text.innerHTML = "";
+    text.innerHTML = `${fName.value} ${lName.value}, ${mob.value},${pass.value}`;
+  });
 };
 getForm();
 
 const getFocus = () => {
-    inputs.forEach((input) =>
-        input.addEventListener("focus", (e) => {
-            e.preventDefault();
-            e.currentTarget.classList.add("anim-focus");
-        }),
-    );
+  inputs.forEach((input) =>
+    input.addEventListener("focus", (e) => {
+      e.preventDefault();
+      e.currentTarget.classList.add("anim-focus");
+    })
+  );
 };
 const removeFocus = () => {
-    inputs.forEach((input) =>
-        input.addEventListener("blur", (e) => {
-            e.preventDefault();
-            e.currentTarget.classList.remove("anim-focus");
-            if (
-                e.currentTarget.value.length < 5 &&
-                e.currentTarget.value.length !== 0
-            ) {
-                e.currentTarget.classList.add("anim-blur");
-            }else{
-                e.currentTarget.classList.remove("anim-blur");
-            }
-            
-        }),
-    );
+  inputs.forEach((input) =>
+    input.addEventListener("blur", (e) => {
+      e.preventDefault();
+      e.currentTarget.classList.remove("anim-focus");
+      if (
+        e.currentTarget.value.length < 5 &&
+        e.currentTarget.value.length !== 0
+      ) {
+        e.currentTarget.classList.add("anim-blur");
+      } else {
+        e.currentTarget.classList.remove("anim-blur");
+      }
+    })
+  );
 };
 getFocus();
 removeFocus();
